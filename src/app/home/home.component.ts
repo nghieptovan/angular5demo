c5ed83792899739754f91b68d1670a492d24ceac
Nghiep To <nghiep.to@terralogic.com>
2018-03-05 05:57:03 +0700

    Data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+
+  btnText: string = 'Add an Item';
+
+  goalText: string = 'My first life goal';
+
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+  
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
