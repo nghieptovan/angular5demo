deb2b359b3f4cf8c7ba0a492becd2e18431e859f
Nghiep To <nghiep.to@terralogic.com>
2018-03-05 05:31:06 +0700

    Template and styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+ // template: `<p>This is my inline template HTML!</p>`,
+ /*styles: [` 
+  p { font-weight: bold; }
+  div { color: gray; }  
+  `]*/ 
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
